8a439351e6744aa1d3fd89240b750e3535b09194
Sai Pradeep Dandem <saipradeep.dandem@gmail.com>
2019-10-29 17:09:14 +1100

    tt

--- test/CustomEventDispatcherDemo.java
+++ test/CustomEventDispatcherDemo.java
@@ -50,83 +50,132 @@ public class CustomEventDispatcherDemo extends Application {
 
     class CustomEventDispatcher implements EventDispatcher {
         private static final long PRESS_DELAY = 300;
+
         private static final long DOUBLE_CLICK_DELAY = 250;
+
         private final EventDispatcher defaultEventDispatcher;
+
         private Timeline pressedTimeline;
+
         private Timeline clickedTimeline;
+
         private boolean isClickedEvent;
 
-        public CustomEventDispatcher(EventDispatcher initial) {
-            this.defaultEventDispatcher = initial;
+        /**
+         * Constructor.
+         *
+         * @param initial Default event dispatcher of a node
+         */
+        public CustomEventDispatcher(final EventDispatcher initial) {
+            defaultEventDispatcher = initial;
         }
 
         @Override
-        public Event dispatchEvent(Event event, EventDispatchChain tail) {
-            final EventType type = event.getEventType();
-            if (type == MouseEvent.MOUSE_PRESSED || type == MouseEvent.MOUSE_RELEASED || type == MouseEvent.MOUSE_CLICKED) {
+        public Event dispatchEvent(final Event event, final EventDispatchChain tail) {
+            final EventType<? extends Event> type = event.getEventType();
+            if (type == MouseEvent.MOUSE_PRESSED
+                    || type == MouseEvent.MOUSE_RELEASED
+                    || type == MouseEvent.MOUSE_CLICKED) {
                 final MouseEvent mouseEvent = (MouseEvent) event;
                 final EventTarget eventTarget = event.getTarget();
+                Event returnEvent = null;
                 if (type == MouseEvent.MOUSE_PRESSED) {
-                    if (clickedTimeline != null && clickedTimeline.getStatus() == Animation.Status.RUNNING) {
-                        return event;
-                    } else {
-                        isClickedEvent = false;
-                        if (pressedTimeline == null) {
-                            final MouseEvent pressedEvent = copy(mouseEvent, mouseEvent.getEventType());
-                            pressedTimeline = new Timeline(new KeyFrame(Duration.millis(PRESS_DELAY), e -> {
-                                Event.fireEvent(eventTarget, pressedEvent);
-                                pressedTimeline = null;
-                            }));
-                            pressedTimeline.play();
-                            return event;
-                        }
-                    }
+                    returnEvent = processMouseEntered(mouseEvent, eventTarget);
                 } else if (type == MouseEvent.MOUSE_RELEASED) {
-                    if (clickedTimeline != null && clickedTimeline.getStatus() == Animation.Status.RUNNING) {
-                        return event;
-                    } else {
-                        if (pressedTimeline != null) {
-                            isClickedEvent = true;
-                            pressedTimeline.stop();
-                            pressedTimeline = null;
-                            return event;
-                        }
-                    }
+                    returnEvent = processMouseReleased(mouseEvent);
                 } else if (type == MouseEvent.MOUSE_CLICKED) {
-                    if (!isClickedEvent) {
-                        return event;
-                    } else {
-                        int count = ((MouseEvent) event).getClickCount();
-                        if (count > 1) {
-                            if (clickedTimeline != null) {
-                                clickedTimeline.stop();
-                                clickedTimeline = null;
-                                final MouseEvent dblClickedEvent = copy(mouseEvent, MOUSE_DBL_CLICKED);
-                                Event.fireEvent(eventTarget, dblClickedEvent);
-                            }
-                            return event;
-                        } else {
-                            if (clickedTimeline == null) {
-                                final MouseEvent clickedEvent = copy(mouseEvent, mouseEvent.getEventType());
-                                clickedTimeline = new Timeline(new KeyFrame(Duration.millis(DOUBLE_CLICK_DELAY), e -> {
-                                    Event.fireEvent(eventTarget, clickedEvent);
-                                    clickedTimeline = null;
-                                }));
-                                clickedTimeline.play();
-                                return event;
-                            }
-                        }
-                    }
+                    returnEvent = processMouseClicked(mouseEvent, eventTarget);
+                }
+
+                if (returnEvent != null) {
+                    return returnEvent;
                 }
             }
             return defaultEventDispatcher.dispatchEvent(event, tail);
         }
 
-        private MouseEvent copy(MouseEvent e, EventType<? extends MouseEvent> eventType) {
-            return new MouseEvent(eventType, e.getSceneX(), e.getSceneY(), e.getScreenX(),
-                    e.getScreenY(), e.getButton(), e.getClickCount(), e.isShiftDown(), e.isControlDown(), e.isAltDown(), e.isMetaDown(), e.isPrimaryButtonDown(), e.isMiddleButtonDown(),
-                    e.isSecondaryButtonDown(), e.isSynthesized(), e.isPopupTrigger(), e.isStillSincePress(), e.getPickResult());
+        /**
+         * Creates a copy of the provided mouse event type with the mouse event.
+         *
+         * @param e         MouseEvent
+         * @param eventType Event type that need to be created
+         * @return New mouse event instance
+         */
+        private MouseEvent copy(final MouseEvent e, final EventType<? extends MouseEvent> eventType) {
+            return new MouseEvent(eventType, e.getSceneX(), e.getSceneY(), e.getScreenX(), e.getScreenY(),
+                    e.getButton(), e.getClickCount(), e.isShiftDown(), e.isControlDown(), e.isAltDown(),
+                    e.isMetaDown(), e.isPrimaryButtonDown(), e.isMiddleButtonDown(),
+                    e.isSecondaryButtonDown(), e.isSynthesized(), e.isPopupTrigger(),
+                    e.isStillSincePress(), e.getPickResult());
+        }
+
+        /**
+         * @param mouseEvent
+         * @param eventTarget
+         * @return
+         */
+        private Event processMouseClicked(final MouseEvent mouseEvent, final EventTarget eventTarget) {
+            if (!isClickedEvent) {
+                return mouseEvent;
+            }
+            if (mouseEvent.getClickCount() > 1) {
+                if (clickedTimeline != null) {
+                    clickedTimeline.stop();
+                    clickedTimeline = null;
+                    final MouseEvent dblClickedEvent = copy(mouseEvent, MOUSE_DBL_CLICKED);
+                    Event.fireEvent(eventTarget, dblClickedEvent);
+                }
+                return mouseEvent;
+            }
+            if (clickedTimeline == null) {
+                final MouseEvent clickedEvent = copy(mouseEvent, mouseEvent.getEventType());
+                clickedTimeline = new Timeline(new KeyFrame(Duration.millis(DOUBLE_CLICK_DELAY), e -> {
+                    Event.fireEvent(eventTarget, clickedEvent);
+                    clickedTimeline = null;
+                }));
+                clickedTimeline.play();
+                return mouseEvent;
+            }
+            return null;
+        }
+
+        /**
+         * @param mouseEvent
+         * @param eventTarget
+         * @return
+         */
+        private Event processMouseEntered(final MouseEvent mouseEvent, final EventTarget eventTarget) {
+            if (clickedTimeline != null && clickedTimeline.getStatus() == Animation.Status.RUNNING) {
+                return mouseEvent;
+            }
+            isClickedEvent = false;
+            if (pressedTimeline == null) {
+                final MouseEvent pressedEvent = copy(mouseEvent, mouseEvent.getEventType());
+                pressedTimeline = new Timeline(new KeyFrame(Duration.millis(PRESS_DELAY), e -> {
+                    Event.fireEvent(eventTarget, pressedEvent);
+                    pressedTimeline = null;
+                }));
+                pressedTimeline.play();
+                return mouseEvent;
+            }
+            return null;
+        }
+
+        /**
+         * @param mouseEvent
+         * @return
+         */
+        private Event processMouseReleased(final MouseEvent mouseEvent) {
+            if (clickedTimeline != null && clickedTimeline.getStatus() == Animation.Status.RUNNING) {
+                return mouseEvent;
+            }
+            if (pressedTimeline != null) {
+                isClickedEvent = true;
+                pressedTimeline.stop();
+                pressedTimeline = null;
+                return mouseEvent;
+            }
+            return null;
         }
     }
-
 }
